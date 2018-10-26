8670ba334bd8ef9e1c77dc3342b92b39d80c2b92
Alisa Lin <aaalisa.lin@gmail.com>
2018-10-26 18:22:03 -0500

    first commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game_3734.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game_3734.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 
 @TeleOp(name="Linear Opmode", group = "teleops???")
